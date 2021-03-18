eb00e1045f3d3735e327353ae6a3d4c9767fbf63
wtfsck <wtfsck@protonmail.com>
2021-03-18 21:48:20 +0100

    Enable lto (gcc), except udis86 which got slower with lto

--- make-all.sh
+++ make-all.sh
@@ -40,29 +40,40 @@ else
         echo "[-] Unable to find cargo (Rust), is it installed?" 1>&2
         exit 1
     fi
+    if [[ ! `which gcc-ar` ]]; then
+        echo "[-] Unable to find gcc-ar, is it installed?" 1>&2
+        exit 1
+    fi
+    if [[ ! `which gcc-ranlib` ]]; then
+        echo "[-] Unable to find gcc-ranlib, is it installed?" 1>&2
+        exit 1
+    fi
 fi
 
 # Build Capstone
 
 echo "[*] Building Capstone ..."
 cd libs/capstone
+mkdir -p build
+cd build
 if [[ "$is_windows" == "y" ]]; then
-    mkdir -p build
-    cd build
-    cmake -DCMAKE_BUILD_TYPE=Release -DCAPSTONE_BUILD_STATIC_RUNTIME=ON -DCAPSTONE_ARCHITECTURE_DEFAULT=OFF -DCAPSTONE_X86_SUPPORT=ON -G "NMake Makefiles" ..
-    nmake
-    cd ../../..
+    cmake -DCMAKE_BUILD_TYPE=Release -DCAPSTONE_BUILD_STATIC_RUNTIME=ON -DCAPSTONE_ARCHITECTURE_DEFAULT=OFF -DCAPSTONE_X86_SUPPORT=ON ..
+    msbuild.exe capstone-static.vcxproj -p:Configuration=Release -p:Platform=x64 -p:WholeProgramOptimization=true
 else
-    export CAPSTONE_ARCHS="x86"
-    $make
-    cd ../..
+    CFLAGS=-flto cmake -DCMAKE_BUILD_TYPE=Release -DCAPSTONE_BUILD_STATIC_RUNTIME=ON -DCAPSTONE_ARCHITECTURE_DEFAULT=OFF -DCAPSTONE_X86_SUPPORT=ON -DCMAKE_AR=$(which gcc-ar) -DCMAKE_RANLIB=$(which gcc-ranlib) ..
+    make capstone-static
 fi
+cd ../../..
 
 # Build Intel XED
 
 echo "[*] Building Intel XED ..."
 cd libs/intelxed
-$python mfile.py --opt=3
+if [[ "$is_windows" == "y" ]]; then
+    $python mfile.py --no-encoder --opt=3 --extra-flags=-GL
+else
+    $python mfile.py --no-encoder --opt=3 --extra-flags=-flto --ar=$(which gcc-ar)
+fi
 cd ../..
 
 # Build diStorm
@@ -77,9 +88,12 @@ else
 fi
 cd libs/distorm/make/${distorm_subdir}
 if [[ "$is_windows" == "y" ]]; then
-    msbuild.exe cdistorm.vcxproj -p:Configuration=clib -p:Platform=x64
+    msbuild.exe cdistorm.vcxproj -p:Configuration=clib -p:Platform=x64 -p:WholeProgramOptimization=true
 else
-    $make
+    sed -i -e 's/ar rs/gcc-ar rs/' Makefile
+    sed -i -e 's/CFLAGS\s*=/CFLAGS +=/' Makefile
+    CFLAGS=-flto make
+    git checkout Makefile
 fi
 cd ../../../..
 
@@ -88,11 +102,15 @@ cd ../../../..
 echo "[*] Building bddisasm ..."
 cd libs/bddisasm
 if [[ "$is_windows" == "y" ]]; then
-    msbuild.exe bddisasm/bddisasm.vcxproj -p:Configuration=Release -p:Platform=x64
+    msbuild.exe bddisasm/bddisasm.vcxproj -p:Configuration=Release -p:Platform=x64 -p:WholeProgramOptimization=true
+    cd ../..
 else
-    $make
+    mkdir -p build
+    cd build
+    CFLAGS=-flto cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_AR=$(which gcc-ar) -DCMAKE_RANLIB=$(which gcc-ranlib) ..
+    make bddisasm
+    cd ../../..
 fi
-cd ../..
 
 # Build udis86
 
@@ -104,25 +122,32 @@ if [[ "$is_windows" == "y" ]]; then
     $python scripts/ud_itab.py docs/x86/optable.xml libudis86/
     # If this fails, change the PlatformToolset version to whatever version of VS you have installed
     #   https://docs.microsoft.com/cpp/build/how-to-modify-the-target-framework-and-platform-toolset#platform-toolset
-    msbuild.exe BuildVS2010/libudis86.vcxproj -p:Configuration=Release -p:Platform=x64 -p:PlatformToolset=v142
+    msbuild.exe BuildVS2010/libudis86.vcxproj -p:Configuration=Release -p:Platform=x64 -p:PlatformToolset=v142 -p:WholeProgramOptimization=true
 else
     ./autogen.sh
-    ./configure --enable-static=yes
-    make
+    # We can't enable lto because it gets extremely slow
+    #     ./configure [...] CFLAGS=-flto AR=$(which gcc-ar) RANLIB=$(which gcc-ranlib) [...]
+    # It's been disabled in bench/udis86/Makefile too
+    ./configure --enable-static=yes PYTHON=$(which $python)
+    make libudis86
 fi
 git checkout scripts/ud_opcode.py
 cd ../..
 
 echo "[*] Building BeaEngine ..."
 cd libs/beaengine
+mkdir -p build
+cd build
+sed -i -e 's/APPEND BEA_FLAGS -fomit-frame-pointer -O2/APPEND BEA_FLAGS -fomit-frame-pointer -O3 -flto/' ../CMakeLists.txt
 # It's decode+disasm unless `-DoptBUILD_LITE=on` is used (decode only), but if we add it, we get errors
-cmake -DoptHAS_OPTIMIZED=on -DoptHAS_SYMBOLS=off -DoptBUILD_64BIT=on .
+cmake -DoptHAS_OPTIMIZED=on -DoptHAS_SYMBOLS=off -DoptBUILD_64BIT=on ..
 if [[ "$is_windows" == "y" ]]; then
-    msbuild.exe BeaEngine.sln -p:Configuration=Release -p:Platform=x64
+    msbuild.exe BeaEngine.sln -p:Configuration=Release -p:Platform=x64 -p:WholeProgramOptimization=true
 else
     make
 fi
-cd ../..
+git checkout ../CMakeLists.txt
+cd ../../..
 
 # Build benchmark tools
 
@@ -133,12 +158,15 @@ cd ../..
 
 echo "[*] Building Zydis benchmark ..."
 cd bench/zydis
-cmake -DCMAKE_BUILD_TYPE=Release .
 if [[ "$is_windows" == "y" ]]; then
-    msbuild.exe bench-zydis.sln -p:Configuration=Release -p:Platform=x64
-    cp Release/*.exe .
-    cp zydis/Release/Zydis.dll .
+    mkdir -p build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release ..
+    msbuild.exe bench-zydis.sln -p:Configuration=Release -p:Platform=x64 -p:WholeProgramOptimization=true
+    cp Release/*.exe ..
+    cd ..
 else
+    CFLAGS=-flto cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_AR=$(which gcc-ar) -DCMAKE_RANLIB=$(which gcc-ranlib) .
     $make
 fi
 cd ../..
